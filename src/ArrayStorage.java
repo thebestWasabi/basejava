12be8ac431bb2399c0b0039bca400a2dce8d0515
theBestWasabi <openpw@icloud.com>
2024-04-26 01:54:29 +0300

    feat: ArrayStorage.java
    Добавил реализацию метода get и delete. Так же добавил приватные sub-methods (indexOf и removeByIndex) для методов get и delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,16 +1,16 @@
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+
+    private Resume[] storage = new Resume[10000];
     private int count;
 
     void clear() {
         // сам storage можно и не чистить, так как его мы выводить не будем (во всяком случае пока)
         this.count = 0;
     }
+
 
     void save(Resume r) {
         if (r == null) {
@@ -26,12 +26,50 @@ public class ArrayStorage {
         this.storage[this.count++] = r;
     }
 
+
     Resume get(String uuid) {
+        if (uuid == null) {
+            throw new IllegalArgumentException("Cannot get null uuid");
+        }
+
+        final var index = indexOf(uuid);
+        if (index != -1) {
+            return this.storage[index];
+        }
         return null;
     }
 
+
     void delete(String uuid) {
+        if (uuid == null) {
+            throw new IllegalArgumentException("Cannot delete null uuid");
+        }
+
+        final var index = indexOf(uuid);
+
+        if (index != -1) {
+            removeByIndex(index);
+        }
     }
+
+
+    private int indexOf(final String uuid) {
+        for (int i = 0; i < this.count; i++) {
+            if (this.storage[i].uuid.equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    private void removeByIndex(final int index) {
+        if (index < this.count - 1) {
+            System.arraycopy(this.storage, index + 1, this.storage, index, this.count - index - 1);
+        }
+        this.count--;
+    }
+
 
     /**
      * @return array, contains only Resumes in storage (without null)
@@ -44,6 +82,7 @@ public class ArrayStorage {
         System.arraycopy(this.storage, 0, result, 0, this.count);
         return result;
     }
+
 
     int size() {
         return 0;
