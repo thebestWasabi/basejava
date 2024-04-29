308de0e7526d60e292b0f9f1779dcaa3894c4ea4
theBestWasabi <openpw@icloud.com>
2024-04-29 08:57:51 +0300

    refactor: Resume.java
    Добавил еще один конструктор

--- src/ru/maxim_khamzin/webapp/model/Resume.java
+++ src/ru/maxim_khamzin/webapp/model/Resume.java
@@ -1,10 +1,15 @@
 package ru.maxim_khamzin.webapp.model;
 
 import java.util.Objects;
+import java.util.UUID;
 
 public class Resume implements Comparable<Resume> {
 
     private final String uuid;
+
+    public Resume() {
+        this(UUID.randomUUID().toString());
+    }
 
     public Resume(final String uuid) {
         this.uuid = uuid;
