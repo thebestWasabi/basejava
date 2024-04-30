a8402b6f70ad2e93de6d5da4d650a952f67604ab
theBestWasabi <openpw@icloud.com>
2024-04-30 12:30:48 +0300

    add: tests
    Написал JUnit4 тесты для AbstractArrayStorageTest & ArrayStorageTest & SortedArrayStorageTest

---
+++ test/ru/maxim_khamzin/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,8 @@
+package ru.maxim_khamzin.webapp.storage;
+
+public class ArrayStorageTest extends AbstractArrayStorageTest {
+
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
+    }
+}
---
+++ test/ru/maxim_khamzin/webapp/storage/SortedArrayStorageTest.java
@@ -0,0 +1,8 @@
+package ru.maxim_khamzin.webapp.storage;
+
+public class SortedArrayStorageTest extends AbstractArrayStorageTest {
+
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
+    }
+}
