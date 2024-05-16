337ad3e2f28db989ddf5d2247c90736dbf0a3e9d
theBestWasabi <openpw@icloud.com>
2024-05-16 03:06:21 +0300

    add: ListStorageTest

---
+++ test/ru/maxim_khamzin/webapp/storage/ListStorageTest.java
@@ -0,0 +1,11 @@
+package ru.maxim_khamzin.webapp.storage;
+
+import static org.junit.Assert.*;
+
+public class ListStorageTest extends AbstractStorageTest {
+
+    public ListStorageTest() {
+        super(new ListStorage());
+    }
+
+}
