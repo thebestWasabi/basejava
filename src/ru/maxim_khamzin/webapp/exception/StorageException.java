7a695754913581e2fd7677c5eeea804c3a1f3afc
theBestWasabi <openpw@icloud.com>
2024-05-27 07:50:50 +0300

    refactor: StorageException

--- src/ru/maxim_khamzin/webapp/exception/StorageException.java
+++ src/ru/maxim_khamzin/webapp/exception/StorageException.java
@@ -4,9 +4,17 @@ public class StorageException extends RuntimeException {
 
     private final String uuid;
 
+    public StorageException(final String message) {
+        this(message, null, null);
+    }
+
     public StorageException(final String message, final String uuid) {
         super(message);
         this.uuid = uuid;
+    }
+
+    public StorageException(final String message, final Exception e) {
+        this(message, null, e);
     }
 
     public StorageException(final String message, final String uuid, final Exception e) {
