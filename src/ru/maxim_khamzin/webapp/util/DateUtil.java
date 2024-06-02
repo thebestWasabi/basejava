d042a9a84040023f4eec941051ee28508e039f1d
theBestWasabi <openpw@icloud.com>
2024-06-02 16:33:56 +0300

    refactor DateUtil

--- src/ru/maxim_khamzin/webapp/util/DateUtil.java
+++ src/ru/maxim_khamzin/webapp/util/DateUtil.java
@@ -3,8 +3,12 @@ package ru.maxim_khamzin.webapp.util;
 import java.time.LocalDate;
 import java.time.Month;
 
-public class DateUtil {
+public final class DateUtil {
+
     public static final LocalDate NOW = LocalDate.of(3000, 1, 1);
+
+    private DateUtil() {
+    }
 
     public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
